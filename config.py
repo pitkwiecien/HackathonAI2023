d673b2fda034116295b63b287daa06633975b299
pitkwiecien <pitkwiecien@gmail.com>
2023-12-09 09:35:32 +0100

    Added config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 PROJECT_REPO_URL = r"https://github.com/pitkwiecien/GitEasyClasses.git"
-PROJECT_REPO_LOCATION = r"P:\Files\Python\llm\_AuxiliaryProjects\GitEasyClasses"
+PROJECT_REPO_LOCATION = r"~/desktop/hackathon/proj1-src/GitEasyClasses"
 PINECONE_INDEX_NAME = "test-index"
